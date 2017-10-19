b8fdf0e07f821a26e28f6a6cadb5d7a43316472f
ironic <hjron@github.com>
2017-10-19 10:09:26 -0700

    finish card test 1, add all card tests to makefile

--- projects/hagenr/dominion/cardtest1.c
+++ projects/hagenr/dominion/cardtest1.c
@@ -2,28 +2,29 @@
 #include "dominion_helpers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "rngs.h"
 
 /*
  * NOTE: card behavior based on module notes from the instructor.
- * NOTE: test set up influenced by the steward sample test case from instructor
+ * NOTE: test setup influenced by the steward sample test case from instructor
  *
  * test suite for the smithy card
  * card behavior: player discards the smithy and adds three cards from their 
  *   deck to their hand. no changes to state of other player and no changes in
  *   the number of cards in the supply piles. 
- * checks
- *   number of smithy cards in hand decremented
- *   deck has 3 fewer cards
- *   hand has 2 more cards
- *   supply piles are unchanged
- *   player 2 cards unchanged
  * test setup: call initializeGame with 2 players, random seed of 10.
- * test 1: call update coins with standard initial game state for player 0,
- *         no bonus. initial hand has 4 coppers, coins should be 4 * 1 = 4
- * test 2: change player 0 hand to have 2 coppers, 1 silver, 1 gold. call
- *         updateCoins with bonus of 1. coins should be 2 + 2 + 3 + 1 = 8
+ * tests
+ *   player 1 deck has 3 fewer cards
+ *   player 1 hand has 2 more cards
+ *   player 1 played 1 card
+ *   player 2 deck unchanged
+ *   coins are unchanged
+ *   player 1 discards 7 cards
+ *   supply pile counjs are unchanged
  */
+
+void printResult(int expected, int actual, char *tmp);
 
 int main (int argc, char** argv) {
     struct gameState pre, post;
@@ -36,44 +37,68 @@ int main (int argc, char** argv) {
 	sea_hag, tribute, smithy};
 
     int i;
+    int expected, actual;
     int mismatch = 0;
+    int numNewCardsInHand = 3;
+    int numPlayedCards = 1;
 
     /* initialize 2 player game with seed 10 */
     initializeGame(numPlayers, k, seed, &pre);
 
-    /* pre.hand[playerOne][0] = smithy; */
-
     /* copy the game state, use post for testing, and compare to pre */
     post = pre;
 
-    printf("test suite for smithy\n");
-    printf("test 1: ");
-    /* printf("before cardEffect hand count pre: %d, post: %d\n", pre.handCount[playerOne], post.handCount[playerOne]); */
-    /* cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus) */
+    printf("test suite for playing smithy\n");
     cardEffect(smithy, choice1, choice2, choice3, &post, handPos, &bonus);
-    printf("hand count pre: %d, post: %d\n", pre.handCount[playerOne], post.handCount[playerOne]);
-    printf("played card count pre: %d, post: %d\n", pre.playedCardCount, post.playedCardCount);
-    printf("deck count pre: %d, post: %d\n", pre.deckCount[playerOne], post.deckCount[playerOne]);
-    printf("coin count pre: %d, post: %d\n", pre.coins, post.coins);
+
+    expected = pre.handCount[playerOne] + numNewCardsInHand - numPlayedCards;
+    actual = post.handCount[playerOne];
+    printResult(expected, actual, "Player 1 hand count");
+
+    expected = pre.deckCount[playerOne] - numNewCardsInHand;
+    actual = post.deckCount[playerOne];
+    printResult(expected, actual, "Player 1 deck count");
+
+    expected = pre.playedCardCount + numPlayedCards;
+    actual = post.playedCardCount;
+    printResult(expected, actual, "Player 1 played card count");
+
+    expected = pre.deckCount[playerTwo];
+    actual = post.deckCount[playerTwo];
+    printResult(expected, actual, "Player 2 deck count");
+
+    expected = pre.coins;
+    actual = post.coins;
+    printResult(expected, actual, "Coin count");
+
     endTurn(&post);
-    printf("discard count pre: %d, post: %d\n", pre.discardCount[playerOne], post.discardCount[playerOne]);
-    printf("deck count pre: %d, post: %d\n", pre.deckCount[playerOne], post.deckCount[playerOne]);
+
+    expected = numNewCardsInHand + pre.handCount[playerOne] - numPlayedCards;
+    actual = post.discardCount[playerOne];
+    printResult(expected, actual, "Player 1 discard count");
+
+    printf("Supply counts: ");
     for (i = 0; i < treasure_map + 1; i++) {
 	if (pre.supplyCount[i] != post.supplyCount[i]) {
 	    mismatch = 1;
 	    break;
 	}
     }
-    if (mismatch) {
-	printf("supply counts don't match: %d\n", i);
+    if (!mismatch) {
+	printf("PASSED\n");
     } else {
-	printf("supply counts match\n");
+	printf("FAILED: Supply counts don't match at card %d\n", i);
     }
 
-
-    /* printf("%s", (actual == expected) ? "PASS" : "FAIL"); */
-    /* printf(" - should have 4 coins for initial hand\n"); */
-
-
     return 0;
+}
+
+void printResult(int expected, int actual, char *tmp) {
+    char *testName = tmp;
+    if (expected == actual) {
+	printf("PASSED: %s", testName);
+    } else {
+	printf("FAILED: %s - expected %d, got %d", testName, expected, actual);
+    }
+    printf("\n");
 }
