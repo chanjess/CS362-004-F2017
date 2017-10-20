580de3ae25ca16892013d6650aa02ca9d51f3e94
ironic <hjron@github.com>
2017-10-20 08:29:19 -0700

    add opponent test to card test 4 (village)

--- projects/hagenr/dominion/cardtest4.c
+++ projects/hagenr/dominion/cardtest4.c
@@ -11,13 +11,13 @@
  * test suite for the village card
  * card behavior: draw 1 card, get 2 more actions
  * test setup: call initializeGame with 2 players, random seed of 10.
- * tests
- *   player 1 deck has 1 fewer card
- *   player 1 hand has 1 more card
- *   player 1 played 1 card
- *   player 1 discards 5 cards
- *   player 1 has 2 more actions
- *   player 2 deck unchanged
+ * 2 tests, one for each player (player 1 is 'player', player 2 is 'opponent'):
+ *   player deck has 1 fewer card
+ *   player hand has 1 more card
+ *   player played 1 card
+ *   player discards 5 cards
+ *   player has 2 more actions
+ *   opponent deck unchanged
  *   coins are unchanged
  *   supply pile counts are unchanged
  */
@@ -27,10 +27,8 @@ void printResult(int expected, int actual, char *tmp);
 int main (int argc, char** argv) {
     struct gameState pre, post;
     int numPlayers = 2;
-    int playerOne = 0;
-    int playerTwo = 1;
-    /* not sure what the choices should be for council room, 0 makes sense */
-    int handPos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+    /* set choices to same as smithy, since they're not used for this card */
+    int handPos = 0, choice1 = -1, choice2 = -1, choice3 = -1, bonus = 0;
     int seed = 10;
     int k[] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
 	sea_hag, tribute, smithy};
@@ -41,6 +39,7 @@ int main (int argc, char** argv) {
     int numNewCardsInHand = 1;
     int numPlayedCards = 1;
     int numNewActions = 2;
+    int playerCount;
 
     /* initialize 2 player game with seed 10 */
     initializeGame(numPlayers, k, seed, &pre);
@@ -49,48 +48,54 @@ int main (int argc, char** argv) {
     post = pre;
 
     printf("test suite for playing village\n");
-    cardEffect(village, choice1, choice2, choice3, &post, handPos, &bonus);
+    for (playerCount = 0; playerCount < numPlayers; playerCount++) {
+	cardEffect(village, choice1, choice2, choice3, &post, handPos, &bonus);
 
-    expected = pre.handCount[playerOne] + numNewCardsInHand - numPlayedCards;
-    actual = post.handCount[playerOne];
-    printResult(expected, actual, "Player 1 hand count");
+	printf("\ntest results for player %d\n", playerCount+1);
+	expected = pre.handCount[playerCount] + numNewCardsInHand - numPlayedCards;
+	actual = post.handCount[playerCount];
+	printResult(expected, actual, "Player hand count");
 
-    expected = pre.deckCount[playerOne] - numNewCardsInHand;
-    actual = post.deckCount[playerOne];
-    printResult(expected, actual, "Player 1 deck count");
+	expected = pre.deckCount[playerCount] - numNewCardsInHand;
+	actual = post.deckCount[playerCount];
+	printResult(expected, actual, "Player deck count");
 
-    expected = pre.playedCardCount + numPlayedCards;
-    actual = post.playedCardCount;
-    printResult(expected, actual, "Player 1 played card count");
+	expected = pre.playedCardCount + numPlayedCards;
+	actual = post.playedCardCount;
+	printResult(expected, actual, "Player played card count");
 
-    expected = pre.numActions + numNewActions;
-    actual = post.numActions;
-    printResult(expected, actual, "Player 1 actions count");
+	expected = pre.numActions + numNewActions;
+	actual = post.numActions;
+	printResult(expected, actual, "Player actions count");
 
-    expected = pre.deckCount[playerTwo];
-    actual = post.deckCount[playerTwo];
-    printResult(expected, actual, "Player 2 deck count");
+	expected = playerCount == 1 ? 
+	    pre.deckCount[1-playerCount] - numNewCardsInHand :
+	    pre.deckCount[1-playerCount];
+	actual = post.deckCount[1-playerCount];
+	printResult(expected, actual, "Opponent deck count");
 
-    expected = pre.coins;
-    actual = post.coins;
-    printResult(expected, actual, "Coin count");
+	expected = pre.coins;
+	actual = post.coins;
+	printResult(expected, actual, "Coin count");
 
-    endTurn(&post);
+	/* have to call endTurn to actually discard */
+	expected = numNewCardsInHand + pre.handCount[playerCount] - numPlayedCards;
+	endTurn(&post);
+	endTurn(&pre);
+	actual = post.discardCount[playerCount];
+	printResult(expected, actual, "Player discard count");
 
-    expected = numNewCardsInHand + pre.handCount[playerOne] - numPlayedCards;
-    actual = post.discardCount[playerOne];
-    printResult(expected, actual, "Player 1 discard count");
-
-    for (i = 0; i < treasure_map + 1; i++) {
-	if (pre.supplyCount[i] != post.supplyCount[i]) {
-	    mismatch = 1;
-	    break;
+	for (i = 0; i < treasure_map + 1; i++) {
+	    if (pre.supplyCount[i] != post.supplyCount[i]) {
+		mismatch = 1;
+		break;
+	    }
 	}
-    }
-    if (!mismatch) {
-	printf("PASSED: Supply counts unchanged\n");
-    } else {
-	printf("FAILED: Supply counts don't match at card %d\n", i);
+	if (!mismatch) {
+	    printf("PASSED: Supply counts unchanged\n");
+	} else {
+	    printf("FAILED: Supply counts don't match at card %d\n", i);
+	}
     }
 
     return 0;
